66d2fb442376b0aefc9b039d40f28db3450841ef
andmana <iam.andmana@gmail.com>
2025-02-20 14:42:23 +0700

    Update ship

--- src/models/Ship.js
+++ src/models/Ship.js
@@ -1,15 +1,13 @@
 const Ship = (_length) => {
-    if (!Number.isInteger(_length)) throw new Error("Ship length must be number");
+    if (!Number.isInteger(_length)) throw new Error("Ship length must be an integer");
     if (_length < 1) throw new Error("Ship length must be greater than 0");
 
     let length = _length;
     let hits = 0;
 
     const hit = () => {
-        if (isSunk) throw new Error("Ship already sunked");
-
+        if (isSunk()) throw new Error("Ship already sunken");
         hits += 1;
-        if (isSunk()) isSunk = true;
     };
 
     const isSunk = () => {
--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -1,8 +1,31 @@
 const Ship = require("../src/models/Ship");
 
 describe("Ship Tests", () => {
-    test("applies the correct length at instantiation", () => {
-        const ship = Ship(3);
+    const ship = Ship(3);
+
+    test("apply the correct length at instantiation", () => {
         expect(ship.getLength()).toBe(3);
+    });
+
+    it("should be sunk when hitted with the same amount of length", () => {
+        ship.hit();
+        expect(ship.isSunk()).toBe(false);
+
+        ship.hit();
+        expect(ship.isSunk()).toBe(false);
+
+        ship.hit();
+        expect(ship.isSunk()).toBe(true);
+    });
+
+    it("throws error when hit sunken ship", () => {
+        expect(() => ship.hit()).toThrow("Ship already sunken");
+    });
+
+    it("should throw an error when put invalid ship length", () => {
+        expect(() => Ship("a")).toThrow("Ship length must be an integer");
+        expect(() => Ship(10.2)).toThrow("Ship length must be an integer");
+        expect(() => Ship(0)).toThrow("Ship length must be greater than 0");
+        expect(() => Ship(-1)).toThrow("Ship length must be greater than 0");
     });
 });
