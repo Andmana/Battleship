99c7832e074acf7d89001490ea9d82db9295d704
andmana <iam.andmana@gmail.com>
2025-02-21 21:59:30 +0700

    update player model

--- src/models/Player.js
+++ src/models/Player.js
@@ -3,8 +3,14 @@ const Gameboard = require("./Gameboard");
 const Player = (_type = "Player") => {
     let gameboard;
     const type = _type;
-    const setGameBoard = (dimension) => Gameboard(dimension);
+    const setGameBoard = () => (gameboard = Gameboard());
+    const getBoard = () => gameboard;
+    const getType = () => type();
     return {
         setGameBoard,
+        getBoard,
+        getType,
     };
 };
+
+module.exports = Player;
