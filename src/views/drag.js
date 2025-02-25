c5693c7dec31134158c807a1e0757dba7519dd35
andmana <iam.andmana@gmail.com>
2025-02-25 22:17:34 +0700

    update drag drop

--- src/views/drag.js
+++ src/views/drag.js
@@ -11,14 +11,16 @@ const drag = () => {
     ships.forEach((ship) => {
         ship.addEventListener("click", (e) => {
             isDragging = true;
+            ship.classList.add("invisible");
 
             movingShip = createGrabShip(
                 ship.dataset.id,
-                parseInt(ship.dataset.length),
+                ship.dataset.length,
                 direction
             );
             movingShip.style.left = `${e.pageX - 20}px`;
             movingShip.style.top = `${e.pageY - 20}px`;
+            movingShip.classList.add("dragging");
 
             body.appendChild(movingShip);
         });
@@ -36,12 +38,31 @@ const drag = () => {
         if (!isDragging) return;
         isDragging = false;
 
+        const boardCell = document.elementFromPoint(e.clientX, e.clientY);
+        if (boardCell && boardCell.classList.contains("gameboard-cell")) {
+            const placedShip = createGrabShip(
+                movingShip.dataset.id,
+                movingShip.dataset.length,
+                direction
+            );
+            placedShip.classList.add("placed-ship");
+            boardCell.appendChild(placedShip);
+            console.log("boardCell", boardCell);
+            console.log("placedShip", placedShip);
+        } else {
+            document.querySelector(".invisible").classList.remove("invisible");
+        }
+
         const draggings = document.querySelectorAll(".dragging");
         draggings.forEach((dragging) => {
-            body.removeChild(dragging);
+            if (dragging.parentNode === body) {
+                body.removeChild(dragging);
+            }
         });
     });
 
+    //rotate
+    // Rotate by type
     document.addEventListener("keydown", function (event) {
         if (event.key === "r" || event.key === "R") {
             direction = direction === "h" ? "v" : "h";
@@ -58,17 +79,17 @@ const drag = () => {
             }
         }
     });
-
+    // by click
     document.querySelector(".game-guidance").addEventListener("click", () => {
         direction = direction === "h" ? "v" : "h";
     });
 };
 
-const createGrabShip = (id, length, direction) => {
+const createGrabShip = (id, _length, direction) => {
+    const length = parseInt(_length);
     const clone = document.createElement("div");
     clone.dataset.id = id;
     clone.dataset.length = length;
-    clone.className = `dragging `;
 
     if (direction === "v") {
         clone.style.height = `${length * 40 + length - 1}px`;
