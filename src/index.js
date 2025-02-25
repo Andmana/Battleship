63e74b2e2ff4e00bbab467edbb636006a6e23cc5
andmana <iam.andmana@gmail.com>
2025-02-25 11:13:26 +0700

    Unnecessary commit

--- src/index.js
+++ src/index.js
@@ -5,3 +5,13 @@ const GameController = require("./GameController");
 // console.log("Hi Webpack");
 
 const controller = GameController();
+
+// const board = document.querySelector(".temporary-board");
+// let innerhtml = "";
+// for (let row = 0; row < 10; row++) {
+//     for (let col = 0; col < 10; col++) {
+//         innerhtml += '<div class="gameboard-cell"></div>';
+//     }
+// }
+
+// board.innerHTML = innerhtml;
